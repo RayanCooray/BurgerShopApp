356ad979a47676d3dce6b682d55abbf9876af71b
rayan <rayancooray123456@gmail.com>
2024-07-09 17:28:45 +0530

    Profile Page Done

--- app/screens/OrderSummaryPage.tsx
+++ app/screens/OrderSummaryPage.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity , Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import RNPickerSelect from 'react-native-picker-select';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -21,31 +28,34 @@ const data = [
   },
 ];
 
-
 export default function OrderSummaryPage() {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  function handlePay() {
+    navigation.navigate("Menu");
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Order Summary</Text>
       <View style={styles.divider}>
         <View style={styles.orderdItems}>
-      {data.slice(0, 1).map((item, index) => (
-          <View key={index} style={styles.menuItem}>
-            <Image source={item.image} style={styles.menuItemImage} />
-            <View style={styles.menuItemInfo}>
-              <Text style={styles.menuItemTitle}>{item.title}</Text>
-              <Text style={styles.menuItemPrice}>{item.price}</Text>
+          {data.slice(0, 1).map((item, index) => (
+            <View key={index} style={styles.menuItem}>
+              <Image source={item.image} style={styles.menuItemImage} />
+              <View style={styles.menuItemInfo}>
+                <Text style={styles.menuItemTitle}>{item.title}</Text>
+                <Text style={styles.menuItemPrice}>{item.price}</Text>
+              </View>
             </View>
-          </View>
-        ))}
-        {data.slice(2, 3).map((item, index) => (
-          <View key={index} style={styles.menuItem}>
-            <Image source={item.image} style={styles.menuItemImage} />
-            <View style={styles.menuItemInfo}>
-              <Text style={styles.menuItemTitle}>{item.title}</Text>
-              <Text style={styles.menuItemPrice}>{item.price}</Text>
+          ))}
+          {data.slice(2, 3).map((item, index) => (
+            <View key={index} style={styles.menuItem}>
+              <Image source={item.image} style={styles.menuItemImage} />
+              <View style={styles.menuItemInfo}>
+                <Text style={styles.menuItemTitle}>{item.title}</Text>
+                <Text style={styles.menuItemPrice}>{item.price}</Text>
+              </View>
             </View>
-          </View>
-        ))}
+          ))}
         </View>
         <View style={styles.finalOrderInformationTab}>
           <Text style={styles.totalHeader}>Item Total</Text>
@@ -67,8 +77,10 @@ export default function OrderSummaryPage() {
         <View style={styles.logoCircle2}></View>
       </View>
       <View>
-        <TouchableOpacity style={styles.Paybutton}>
-          <Text style={styles.buttonText}>Proceed To Pay</Text>
+        <TouchableOpacity onPress={handlePay} style={styles.Paybutton}>
+          <Text style={styles.buttonText} onPress={handlePay}>
+            Proceed To Pay
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -76,171 +88,171 @@ export default function OrderSummaryPage() {
 }
 
 const styles = StyleSheet.create({
-  FinalTotalPriceHeader:{
+  FinalTotalPriceHeader: {
     fontSize: 17,
-    fontWeight : '500',
-    color : '#FFAF32',
-    position : 'absolute',
-    left : 280,
-    top : 140
+    fontWeight: "500",
+    color: "#FFAF32",
+    position: "absolute",
+    left: 280,
+    top: 140,
   },
-  TaxPriceHeader:{
+  TaxPriceHeader: {
     fontSize: 17,
-      fontWeight : '500',
-      color : '#FFAF32',
-      position : 'absolute',
-      left : 280,
-      top : 100
+    fontWeight: "500",
+    color: "#FFAF32",
+    position: "absolute",
+    left: 280,
+    top: 100,
   },
-  DiscountPriceHeader:{
-      fontSize: 17,
-      fontWeight : '500',
-      color : '#FFAF32',
-      position : 'absolute',
-      left : 280,
-      top : 60
-  },
-  FinalPriceHeader:{
+  DiscountPriceHeader: {
     fontSize: 17,
-    fontWeight : '500',
-    color : '#FFAF32',
-    position : 'absolute',
-    left : 280,
-    top : 20
+    fontWeight: "500",
+    color: "#FFAF32",
+    position: "absolute",
+    left: 280,
+    top: 60,
   },
-  FinalTotalHeader:{
+  FinalPriceHeader: {
     fontSize: 17,
-    fontWeight : '500',
-    color : '#A6A6A6',
-    position : 'absolute',
-    left : 15,
-    top : 140
+    fontWeight: "500",
+    color: "#FFAF32",
+    position: "absolute",
+    left: 280,
+    top: 20,
   },
-  TaxHeader:{
+  FinalTotalHeader: {
     fontSize: 17,
-    fontWeight : '500',
-    color : '#A6A6A6',
-    position : 'absolute',
-    left : 15,
-    top : 100
+    fontWeight: "500",
+    color: "#A6A6A6",
+    position: "absolute",
+    left: 15,
+    top: 140,
   },
-  DiscountHeader : {
+  TaxHeader: {
     fontSize: 17,
-    fontWeight : '500',
-    color : '#A6A6A6',
-    position : 'absolute',
-    left : 15,
-    top : 60
+    fontWeight: "500",
+    color: "#A6A6A6",
+    position: "absolute",
+    left: 15,
+    top: 100,
+  },
+  DiscountHeader: {
+    fontSize: 17,
+    fontWeight: "500",
+    color: "#A6A6A6",
+    position: "absolute",
+    left: 15,
+    top: 60,
   },
   totalHeader: {
     fontSize: 17,
-    fontWeight : '500',
-    color : '#A6A6A6',
-    position : 'absolute',
-    left : 15,
-    top : 20
+    fontWeight: "500",
+    color: "#A6A6A6",
+    position: "absolute",
+    left: 15,
+    top: 20,
   },
-  finalOrderInformationTab : {
-      position : 'absolute',
-      width : '100%',
-      height : '48%',
-      // backgroundColor : 'rgba(36, 35, 37, 0.2)',
-      top : 235,
+  finalOrderInformationTab: {
+    position: "absolute",
+    width: "100%",
+    height: "48%",
+    // backgroundColor : 'rgba(36, 35, 37, 0.2)',
+    top: 235,
   },
-  orderdItems :{
-    height : '50%'
+  orderdItems: {
+    height: "50%",
   },
   divider: {
-    position : 'absolute',
-    width : '100%',
-    height : '65%',
-    backgroundColor : 'rgba(207, 216, 220, 0.2)',
-    top : 50,
-    left : 20,
-    bottom : 0,
-    margin : 'auto',
+    position: "absolute",
+    width: "100%",
+    height: "65%",
+    backgroundColor: "rgba(207, 216, 220, 0.2)",
+    top: 50,
+    left: 20,
+    bottom: 0,
+    margin: "auto",
   },
   logoCircle2: {
-    backgroundColor : '#FFAF32',
-    width : 30,
-    height : 30,
-    borderRadius : 100,
-    top : 40,
-    right : 169,
+    backgroundColor: "#FFAF32",
+    width: 30,
+    height: 30,
+    borderRadius: 100,
+    top: 40,
+    right: 169,
   },
   logoCircle: {
-    backgroundColor : 'white',
-    width : 30,
-    height : 30,
-    borderRadius : 100,
-    top : 40,
-    right : 160,
+    backgroundColor: "white",
+    width: 30,
+    height: 30,
+    borderRadius: 100,
+    top: 40,
+    right: 160,
   },
   cardType: {
     fontSize: 16,
-    color: 'white',
+    color: "white",
     top: 40,
     right: 165,
-    position: 'relative',
+    position: "relative",
   },
   cardNumber: {
     fontSize: 20,
-    fontWeight: 'light',
-    color: 'white',
+    fontWeight: "light",
+    color: "white",
     bottom: 40,
     right: 36,
-    position: 'relative',
+    position: "relative",
   },
   cardAmount: {
     fontSize: 40,
-    fontWeight: 'light',
-    color: 'white',
+    fontWeight: "light",
+    color: "white",
     right: 90,
-    top : 30,
-    position : 'relative',
+    top: 30,
+    position: "relative",
   },
   cardheader: {
     fontSize: 20,
-    fontWeight: 'light',
+    fontWeight: "light",
     bottom: 40,
     right: 16,
-    color: 'white',
+    color: "white",
   },
   cardPayment: {
-    position: 'relative',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#242325',
+    position: "relative",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#242325",
     padding: 40,
     top: 450,
     borderRadius: 5,
-    shadowColor: '#000',
+    shadowColor: "#000",
   },
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   header: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
-  Paybutton : {
-    position: 'absolute',
-    width: '100%',
+  Paybutton: {
+    position: "absolute",
+    width: "100%",
     height: 50,
     top: 459,
-    backgroundColor: '#FFAF32',
+    backgroundColor: "#FFAF32",
     padding: 15,
     borderRadius: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   menuItems: {
     flex: 1,
--- app/screens/ProfilePage.tsx
+++ app/screens/ProfilePage.tsx
@@ -1,67 +1,151 @@
-import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet ,SafeAreaView} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function ProfilePage() {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  function handleEdit() {
+    navigation.navigate("Menu");
+  }
+
+  const handleSubmit = () => {
+    if (!firstName || !lastName || !email || !password) {
+      setError("All fields are required");
+      return;
+    }
+    // Handle form submission
+    console.log("Form submitted", { firstName, lastName, email, password });
+    setError(null);
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.decorator}></View>
-      <Text style={styles.heading}>Profile</Text>
-      <Text style={styles.heading2}>Edit Profile</Text>
-    
+    <View style={styles.container}>
+      <Ionicons
+        name="arrow-back-circle-outline"
+        size={24}
+        color="black"
+        onPress={() => navigation.navigate("Home")}
+      />
+      <Text style={styles.header}>Profile</Text>
+
+      <View>
+        <Image
+          source={require("../../assets/img.jpg")}
+          style={styles.profileImage}
+        />
+      </View>
+      <TouchableOpacity style={styles.pen}>
+        <View>
+          <AntDesign
+            name="folderopen"
+            style={styles.penIcon}
+            size={24}
+            color="black"
+          />
+        </View>
+      </TouchableOpacity>
       <TextInput
         style={styles.input}
-        placeholder="Name"
+        placeholder="First Name"
+        value={firstName}
+        onChangeText={setFirstName}
       />
       <TextInput
         style={styles.input}
-        placeholder="Email Address"
+        placeholder="Last Name"
+        value={lastName}
+        onChangeText={setLastName}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Phone Number"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Date of Birth"
-      />
-      <Button title="Save Changes" onPress={() => console.log('Changes saved!')} />
-    </SafeAreaView>
+      <TextInput style={styles.input} placeholder="Address" />
+      <TextInput style={styles.input} placeholder="Province" />
+      <TextInput style={styles.input} placeholder="Number" />
+      <TouchableOpacity style={styles.payButton} onPress={handleEdit}>
+        <Text style={styles.buttonText}>Edit Changes</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.joionedDate}>Joined Since 2024</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  decorator: {
-    width: 500,
-    height: 300,
-    backgroundColor: '#FFAF32',
-    borderBottomRightRadius : 300,
-    borderBottomLeftRadius : 300,
-    alignSelf: 'center',
+  penIcon: {
+    position: "absolute",
+    left: 10,
+    top: 7,
   },
-  container: {
-    width: '90%',
-    margin: 20,
-    flex: 1,
-    padding: 16,
+  pen: {
+    position: "absolute",
+    left: 230,
+    top: 200,
+    width: 40,
+    height: 40,
+    backgroundColor: "#FFAF32",
+    borderRadius: 100,
   },
-  heading2: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 16,
+  profileImage: {
+    position: "absolute",
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    left: 100,
   },
-  heading: {
-    fontSize: 20,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    marginBottom: 16,
-    bottom: 290
+  joionedDate: {
+    fontSize: 17,
+    fontWeight: "500",
+    color: "black",
+    position: "absolute",
+    left: 15,
+    top: 700,
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 16,
+    borderRadius: 50,
+    top: 170,
+
     paddingHorizontal: 8,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  header: {
+    left: 30,
+    bottom: 25,
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  payButton: {
+    backgroundColor: "#FFAF32",
+    padding: 15,
+    borderRadius: 30,
+    top: 170,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
